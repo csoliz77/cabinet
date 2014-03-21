8860d9a7214a9613ffb19f33a400bb13107a9037
Carlos Soliz <csoliz7@gmail.com>
2014-03-21 15:20:48 -0700

    Adding new files

---
+++ app/controllers/FurnitureController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlos
+ * Date: 3/20/14
+ * Time: 12:10 PM
+ */
+
+class FurnitureController extends Controller {
+
+    public function index(){
+        return View::make('furniture')->withTitle('Furniture')->with('section', Section::where('category','=','furniture')->get());
+    }
+}
+
+
+
