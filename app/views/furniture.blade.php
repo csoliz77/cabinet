2d4a8d9f1979b59dcb15d84fd7b9076bcfdce90b
Carlos Soliz <csoliz7@gmail.com>
2014-03-31 14:58:02 -0700

    Working on pagination. Basic style

--- app/views/furniture.blade.php
+++ app/views/furniture.blade.php
@@ -13,8 +13,11 @@
 <div class="boxes" id="page_{{$i->id}}"><a href="assets/imgs/{{ $i->filename }}" rel="lightbox-furniture" title="{{$i->caption}}"> <img src="assets/imgs/{{ $i->thumbs }}"></a></div>
 @endforeach
 </div>
+<div class="breakline"></div>
+<div class="text-center">
+{{ $images->links() }}
 
-
+</div>
 <script>
 $(function(){
     $("#sortable").sortable({
